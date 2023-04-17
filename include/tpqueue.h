c6a55b2718f8892893685a71ad664ebff56458c6
PolliV <77505747+PolliV@users.noreply.github.com>
2023-04-17 20:26:43 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -5,47 +5,47 @@
 
 template<typename T, int size>
 class TPQueue {
-private:
-	T* arr;
-	int head, tail;
-	int count;
-public:
-	TPQueue() : head(0), tail(0), count(0) { arr = new T[size]; }
-	bool IsEmpty() const {
-		return count == 0;	
+  private:
+    T* arr;
+    int head, tail;
+    int count;
+    
+  public:
+    TPQueue() : head(0), tail(0), count(0) { arr = new T[size]; }
+    bool IsEmpty() const {
+      return count == 0;
+    }
+    bool IsFull() const {
+      return size == count;
+    }
+    const T& pop() {
+      if (IsEmpty()) {
+	throw std::string("Empty!");
+      } else {
+	count--;
+	return arr[first++ % size];
+      }
+    }
+    void Push() {
+      if (IsFull()) {
+	throw std::string("Full!");
+      } else {
+        int len = tail;
+	arr[tail % size] = value;
+	T temp = arr[len % size];
+	while (arr[len % size].prior > arr[(len - 1) % size].prior && len > head) {
+	  temp = arr[len % size];
+	  arr[len % size] = arr[(len - 1) % size];
+	  arr[(len - 1) % size] = temp;
+	  len--;
 	}
-	bool IsFull() const {
-		return size == count;
-	}
-	const T& pop() {
-		if (IsEmpty()) {
-			throw std::string("Empty!");
-		}
-		else {
-			count--;
-			return arr[first++ % size];
-		}
-	}
-	void Push() {
-		if (IsFull()) {
-			throw std::string("Full!");
-		} else {
-			int len = tail;
-			arr[tail % size] = value;
-			T temp = arr[len % size];
-			while (arr[len % size].prior > arr[(len - 1) % size].prior && len > head) {
-				temp = arr[len % size];
-				arr[len % size] = arr[(len - 1) % size];
-				arr[(len - 1) % size] = temp;
-				len--;
-			}
-			count++;
-			tail++;
-		}
-	}
-	~TPQueue() {
-		delete[] arr;
-	}
+	count++;
+	tail++;
+       }
+      }
+    ~TPQueue() {
+      delete[] arr;
+    }
 };
 
 struct SYM {
